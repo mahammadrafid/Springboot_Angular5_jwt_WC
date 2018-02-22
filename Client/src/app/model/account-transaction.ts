04960b538944864297a2e701eb22e8c8e096b159
mahammadrafid <30318491+mahammadrafid@users.noreply.github.com>
2018-02-22 18:09:11 +0530

    initial commit

--- Client/src/app/model/account-transaction.ts
+++
@@ -1,9 +0,0 @@
-import {TransactionCategory} from "./transaction-category";
-
-export class AccountTransaction {
-  constructor(public transactionDate: Date,
-              public amount: number,
-              public note: string,
-              public transactionCategory: TransactionCategory) {
-  }
-}
